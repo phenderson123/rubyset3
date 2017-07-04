93c87fe0cd014b4f7de74333a37965ad6122da90
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-07-04 11:20:54 -0400

    first commit

---
+++ super.rb
@@ -0,0 +1,59 @@
+puts "Hello my friend how has you day gone?"
+
+name= gets.chomp
+
+if name == "bad"
+
+puts " Sorry to hear that."
+
+elsif name == "good"
+
+puts " Thats awesome, really glad you are doing well."
+
+elsif name == "so so"
+
+puts "It will get better as it goes on."
+
+end
+
+puts "Well that enough of the small talk, who wins the Superbowl."
+
+name =gets.chomp
+
+if name == "patriots"
+
+puts "That's the winner right there !"
+
+elsif name == "falcons"
+
+puts "well you know they suck lol!"
+
+end
+
+puts" Is this Ryan's best year at quarter back? y or n"
+
+name= gets.chomp
+
+if name == "y"
+
+puts "about time he got it right lol"
+
+elsif name == "n"
+
+puts "don't hate on the guy, he really is a good quarterback lol!"
+
+end
+
+puts "What city do you think is going to be hype?"
+
+name= gets.chomp
+
+if name =="Atlanta"
+
+puts "Thats ATL shaty lol"
+
+elsif name == "Boston"
+
+puts "The champ is here"
+
+end
